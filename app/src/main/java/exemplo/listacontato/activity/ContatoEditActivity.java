9f9cc0b52e6d6ebb92f8f39a77db51873f52daaa
Carlos Genuino <carloxsgenuino3@live.com>
2018-06-10 00:04:48 -0300

    Adição da funcionalidade de tirar foto

--- app/src/main/java/exemplo/listacontato/activity/ContatoEditActivity.java
+++ app/src/main/java/exemplo/listacontato/activity/ContatoEditActivity.java
@@ -1,7 +1,13 @@
 package exemplo.listacontato.activity;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +23,7 @@ import exemplo.listacontato.model.Contato;
 public class ContatoEditActivity extends AppCompatActivity {
 
     private ImageButton foto;
+    private View layout;
     private TextView nome;
     private TextView endereco;
     private TextView telefone;
@@ -24,6 +31,9 @@ public class ContatoEditActivity extends AppCompatActivity {
     private TextView email;
     private TextView referencia;
     private Contato contato;
+
+    public final int CAMERA = 1;
+    public final int GALERIA = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +47,8 @@ public class ContatoEditActivity extends AppCompatActivity {
         celular = findViewById(R.id.celularContato);
         email = findViewById(R.id.emailContato);
         referencia = findViewById(R.id.referenciaContato);
+        layout = findViewById(R.id.activity_contato_edit_layout);
+
         Button buttonSalvar = findViewById(R.id.buttonSalvarContato);
         buttonSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -93,10 +105,50 @@ public class ContatoEditActivity extends AppCompatActivity {
     }
 
     private void tirarFoto(){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            requestCameraPermission();
+        }else{
+            showCamera();
+        }
+    }
 
+    private void showCamera(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA);
+    }
+
+    private void requestCameraPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)){
+            Snackbar.make(layout,R.string.camera_permission, Snackbar.LENGTH_INDEFINITE)
+            .setAction(R.string.ok, new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ActivityCompat.requestPermissions(ContatoEditActivity.this, new String[]{Manifest.permission.CAMERA,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, CAMERA);
+                }
+            }).show();
+        }else{
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, CAMERA);
+        }
     }
 
     private void carregarGaleria(){
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case CAMERA :
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    tirarFoto();
+                }
+                break;
+            case GALERIA :
+                
+                break;
+        }
 
     }
 }
